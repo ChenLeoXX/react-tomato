6f5f181968267ea38e0ec0117882c01be976626c
leo <479711970@QQ.com>
2019-10-11 21:56:28 +0800

    完成登录注册

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,21 +1,70 @@
-import * as React from 'react'
-
-interface PropsIF {
+import * as React from 'react';
+import { Input, Icon,Button ,message} from 'antd';
+import {Link, RouteComponentProps} from 'react-router-dom'
+import api from '../../config/axios'
+import '../SignUp/SignUp.scss'
+import logo from '../../pomotodo_cn.png';
+interface PropsIF extends RouteComponentProps<any>{
 
 }
-
 interface StateIF {
-
+    account:String,
+    password:String,
 }
 
-export default class Login extends React.Component<PropsIF, StateIF> {
-    constructor(props: PropsIF) {
+export default class SignUp extends React.Component<PropsIF, StateIF> {
+    constructor(props:PropsIF) {
         super(props)
+        this.state = {
+            account:'',
+            password:'',
+        }
     }
+    onInputChange = (key:keyof StateIF,e:React.ChangeEvent<HTMLInputElement>)=>{
+        const newState = {}
+        newState[key] = e.target.value
+        this.setState(newState)
+    }
+    signUp = async ()=>{
+        const {account,password} = this.state
+        try {
+            await api.post('/sign_in/user',{
+                account,
+                password,
+            }).then(res=> {
+                if (res.status === 200) {
+                    message.success('登录成功')
+                    this.props.history.push('/')
+                }
+            })
+        }catch (e) {
+            let errMsg = e.response.data.errors
+            if(errMsg){
+                message.error(errMsg)
+            }
+        }
 
+    }
+    validate = ()=>{
+        this.signUp()
+    }
     render() {
         return (
-            <h2>Login</h2>
+            <div id="sign-up">
+                <div className="logo">
+                    <img src={logo} alt=""/>
+                </div>
+                <h1>登录</h1>
+                <Input
+                    placeholder="输入账号"
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    onChange={(e)=>this.onInputChange('account',e)}
+                    allowClear
+                />
+                <Input.Password placeholder="输入密码" onChange={(e)=>this.onInputChange('password',e)}/>
+                <Button onClick={this.validate} type="danger" size='large'>登录</Button>
+                <p className='tip'>还没有账号？立即<Link to='signUp'>注册</Link></p>
+            </div>
         );
     }
 }
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import { Input, Icon,Button ,message} from 'antd';
+import {Link} from 'react-router-dom'
 import api from '../../config/axios'
+import './SignUp.scss'
+import logo from '../../pomotodo_cn.png';
 interface PropsIF {
 
 }
@@ -31,21 +34,27 @@ export default class SignUp extends React.Component<PropsIF, StateIF> {
     }
     signUp = async ()=>{
         const {account,password,passwordConfirmation} = this.state
-        try{
-            await api.post('/sign_up/user',{
-                password_confirmation:passwordConfirmation,
-                account,
-                password,
-            }).then(res=>{
-                if(res.status === 200){
-                    message.success('注册成功')
+            try {
+                await api.post('/sign_up/user',{
+                    password_confirmation:passwordConfirmation,
+                    account,
+                    password,
+                }).then(res=> {
+                    if (res.status === 200) {
+                        message.success('注册成功')
+                    }
+                })
+            }catch (e) {
+                let errMsg = e.response.data.errors
+                if(errMsg['account']){
+                    message.error(`account ${errMsg['account'][0]}`)
+                }else if(errMsg['password']){
+                    message.error(`password ${errMsg['account'][0]}`)
                 }else{
-                    message.error('注册成功')
+                    message.error(`password ${errMsg['password_confirmation'][0]}`)
                 }
-            })
-        }catch (e) {
-            throw new Error(e)
-        }
+            }
+
     }
     validate = ()=>{
         const {password,passwordConfirmation} = this.state
@@ -57,7 +66,11 @@ export default class SignUp extends React.Component<PropsIF, StateIF> {
     }
     render() {
         return (
-            <div className={'sign-up'}>
+            <div id="sign-up">
+                <div className="logo">
+                    <img src={logo} alt=""/>
+                </div>
+                <h1>注册</h1>
                 <Input
                     placeholder="输入账号"
                     prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -66,7 +79,8 @@ export default class SignUp extends React.Component<PropsIF, StateIF> {
                 />
                 <Input.Password placeholder="输入密码"  onChange={this.onPasswordChange}/>
                 <Input.Password placeholder="确认密码" onChange={this.onConfirmChange}/>
-                <Button onClick={this.validate}>注册</Button>
+                <Button onClick={this.validate} type="danger" size='large'>注册</Button>
+                <p className='tip'>如果你已有账号,可立即<Link to='login'>登录</Link></p>
             </div>
         );
     }
